fdf52ac938f98e2909ac2961a502fe6136666487
Vijaykumar Yadav <126179477+Vijay1234667@users.noreply.github.com>
2024-05-16 17:33:01 +0530

    media query add in contact section

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
 
-window.addEventListener("load", function(){
-  setTimeout(
-      function open(event){
-          document.querySelector(".modal").style.
-          display = "block";
-      },
-      2000
-  )
-});
-document.querySelector(".close").addEventListener
-("click", function(){
-  document.querySelector(".modal").style.display = "none";
+// window.addEventListener("load", function(){
+//   setTimeout(
+//       function open(event){
+//           document.querySelector(".modal").style.
+//           display = "block";
+//       },
+//       2000
+//   )
+// });
+// document.querySelector(".close").addEventListener
+// ("click", function(){
+//   document.querySelector(".modal").style.display = "none";
 
-});
+// });
